bae4aab0a0b4fa6df6c188f9e03226b64919f7e6
Kerensivira <92549819+Kerensivira@users.noreply.github.com>
2021-11-19 13:33:26 -0500

    r

--- script.js
+++ script.js
@@ -6,133 +6,133 @@ function convertir() {
     }
         // capturar el valor de "elIdCalcular": elIdCalcular.value
 
-        const getCalcular = () => {
-            let elIdCalcular = document.getElementById("calcular").value;
-            document.getElementById("calcular").innerHTML = inputValue
+const getCalcular = () => {
+        let elIdCalcular = document.getElementById("calcular").value;
+        document.getElementById("calcular").innerHTML = inputValue
+    }
+    // capturar la moneda inicial (local)
+
+    const getDivisas1 = () => {
+        let elIdDivisas1 = document.getElementById("Divisas1").value;
+        document.getElementById("Divisas1").innerHTML = inputValue
+    }
+
+    // captura la moneda final (destino)
+
+    const getDivisas2 = () => {
+        let elIdDivisas1 = document.getElementById("Divisas2").value;
+        document.getElementById("Divisas2").innerHTML = inputValue
+
+        let valore = ["resultado", "COP", "EUR", "GBP", "MXN", "USD"];
+        let valore = parseInt(document.getElementById("monto").value);
+        let resultado = 0;
+        let COP = 10;
+        let EUR = 20;
+        let GBP = 10;
+        let MXN = 20;
+        let USD = 50;
+    }
+
+    //COP A TODAS LAS MONEDAS.
+
+    if (document.getElementById(COP).Checked) {
+        resultado = valore / EUR;
+        alert("el cambio de COP a EUR es:" + resultado)
+    }
+    else if (document.getElementById(EUR).Checked) {
+        resultado = valore / COP;
+        alert("el cambio de EUR a COP es:" + resultado)
+    }
+
+    if (document.getElementById(COP).Checked) {
+        resultado = valore / COP;
+        alert("el cambio de COP a GBP es:" + resultado)
+    }
+    else if (document.getElementById(GBP).Checked) {
+        resultado = valore / COP;
+        alert("el cambio de GBP a COP es:" + resultado)
+    }
+    if (document.getElementById(COP).Checked) {
+        resultado = valore / GBP;
+        alert("el cambio de COP a GBP es:" + resultado)
+    }
+    else if (document.getElementById(GBP).Checked)
+        resultado = valore / COP;
+    alert("el cambio de GBP A COP es:" + resultado)
+
+    {
+        if (document.getElementById(COP).Checked) {
+            resultado = valore / MXN;
+            alert("el cambio de COP a MXN es:" + resultado)
         }
-            // capturar la moneda inicial (local)
+        else if (document.getElementById(MXN).Checked) {
+            resultado = valore / COP;
+            alert("el cambio de MXN a COP es:" + resultado)
+        }
+        if (document.getElementById(COP).Checked) {
+            resultado = valore / USD;
+            alert("el cambio de COP a USD es:" + resultado)
+        }
+        else if (document.getElementById(USD).Checked) {
+            resultado = valore / COP;
+            alert("el cambio de USD a COP es:" + resultado)
+        }
 
-            const getDivisas1 = () => {
-                let elIdDivisas1 = document.getElementById("Divisas1").value;
-                document.getElementById("Divisas1").innerHTML = inputValue
-            }
+        //EUR A TODAS LAS MONEDAS 
 
-            // captura la moneda final (destino)
+        if (document.getElementById(EUR).Checked) {
+            resultado = valore / GBP;
+            alert("el cambio de EUR a GBP es:" + resultado)
+        }
+        else if (document.getElementById(GBP).Checked) {
+            resultado = valore / GBP;
+            alert("el cambio de GBP a EUR es:" + resultado)
+        }
 
-            const getDivisas2 = () => {
-                let elIdDivisas1 = document.getElementById("Divisas2").value;
-                document.getElementById("Divisas2").innerHTML = inputValue
+        if (document.getElementById(EUR).Checked) {
+            resultado = valore / MXN;
+            alert("el cambio de EUR a MXN es:" + resultado)
+        }
+        else if (document.getElementById(MXN).Checked) {
+            resultado = valore / EUR;
+            alert("el cambio de MXN a EUR es:" + resultado)
+        }
+        if (document.getElementById(EUR).Checked) {
+            resultado = valore / USD;
+            alert("el cambio de EUR a USD es:" + resultado)
+        }
+        else if (document.getElementById(USD).Checked)
+            resultado = valore / EUR;
+        alert("el cambio de USD A EUR es:" + resultado)
 
-            let valore = ["resultado", "COP", "EUR", "GBP", "MXN", "USD"];
-            let valore = parseInt(document.getElementById("monto").value);
-            let resultado = 0;
-            let COP = 10;
-            let EUR = 20;
-            let GBP = 10;
-            let MXN = 20;
-            let USD = 50;
-            }
+        //GBP A TODAS LAS MONEDAS.
 
-            //COP A TODAS LAS MONEDAS.
-
-            if (document.getElementById(COP).Checked) {
-                resultado = valore / EUR;
-                alert("el cambio de COP a EUR es:" + resultado)
-            }
-            else if (document.getElementById(EUR).Checked) {
-                resultado = valore / COP;
-                alert("el cambio de EUR a COP es:" + resultado)
-            }
-
-            if (document.getElementById(COP).Checked) {
-                resultado = valore / COP;
-                alert("el cambio de COP a GBP es:" + resultado)
-            }
-            else if (document.getElementById(GBP).Checked) {
-                resultado = valore / COP;
-                alert("el cambio de GBP a COP es:" + resultado)
-            }
-            if (document.getElementById(COP).Checked) {
-                resultado = valore / GBP;
-                alert("el cambio de COP a GBP es:" + resultado)
-            }
-            else if (document.getElementById(GBP).Checked)
-                resultado = valore / COP;
-            alert("el cambio de GBP A COP es:" + resultado)
-
-            {
-                if (document.getElementById(COP).Checked) {
-                    resultado = valore / MXN;
-                    alert("el cambio de COP a MXN es:" + resultado)
-                }
-                else if (document.getElementById(MXN).Checked) {
-                    resultado = valore / COP;
-                    alert("el cambio de MXN a COP es:" + resultado)
-                }
-                if (document.getElementById(COP).Checked) {
-                    resultado = valore / USD;
-                    alert("el cambio de COP a USD es:" + resultado)
-                }
-                else if (document.getElementById(USD).Checked) {
-                    resultado = valore / COP;
-                    alert("el cambio de USD a COP es:" + resultado)
-                }
-            
-            //EUR A TODAS LAS MONEDAS 
-
-            if (document.getElementById(EUR).Checked) {
-                resultado = valore / GBP;
-                alert("el cambio de EUR a GBP es:" + resultado)
-            }
-            else if (document.getElementById(GBP).Checked) {
-                resultado = valore / GBP;
-                alert("el cambio de GBP a EUR es:" + resultado)
-            }
-
-            if (document.getElementById(EUR).Checked) {
+        {
+            if (document.getElementById(GBP).Checked) {
                 resultado = valore / MXN;
-                alert("el cambio de EUR a MXN es:" + resultado)
+                alert("el cambio de GBP a MXN es:" + resultado)
             }
             else if (document.getElementById(MXN).Checked) {
-                resultado = valore / EUR;
-                alert("el cambio de MXN a EUR es:" + resultado)
+                resultado = valore / GBP;
+                alert("el cambio de MXN a GBP es:" + resultado)
             }
-            if (document.getElementById(EUR).Checked) {
+            if (document.getElementById(GBP).Checked) {
                 resultado = valore / USD;
-                alert("el cambio de EUR a USD es:" + resultado)
-            }
-            else if (document.getElementById(USD).Checked)
-                resultado = valore / EUR;
-            alert("el cambio de USD A EUR es:" + resultado)
-
-            //GBP A TODAS LAS MONEDAS.
-
-            {
-                if (document.getElementById(GBP).Checked) {
-                    resultado = valore / MXN;
-                    alert("el cambio de GBP a MXN es:" + resultado)
-                }
-                else if (document.getElementById(MXN).Checked) {
-                    resultado = valore / GBP;
-                    alert("el cambio de MXN a GBP es:" + resultado)
-                }
-                if (document.getElementById(GBP).Checked) {
-                    resultado = valore / USD;
-                    alert("el cambio de GBP a USD es:" + resultado)
-                }
-                else if (document.getElementById(USD).Checked) {
-                    resultado = valore / GBP;
-                    alert("el cambio de USD a GBP es:" + resultado)
-                }
-            }
-
-            //MXN A TODAS LAS MONEDAS
-
-            if (document.getElementById(MXN).Checked) {
-                resultado = valore / USD;
-                alert("el cambio de MXN a USD es:" + resultado)
+                alert("el cambio de GBP a USD es:" + resultado)
             }
             else if (document.getElementById(USD).Checked) {
-                resultado = valore / MXN;
-                alert("el cambio de USD a MXN es:" + resultado)
+                resultado = valore / GBP;
+                alert("el cambio de USD a GBP es:" + resultado)
             }
+        }
+
+        //MXN A TODAS LAS MONEDAS
+
+        if (document.getElementById(MXN).Checked) {
+            resultado = valore / USD;
+            alert("el cambio de MXN a USD es:" + resultado)
+        }
+        else if (document.getElementById(USD).Checked) {
+            resultado = valore / MXN;
+            alert("el cambio de USD a MXN es:" + resultado)
+        }
