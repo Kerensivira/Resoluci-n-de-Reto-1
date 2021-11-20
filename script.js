ce0e06d8f437dbb974e4b8def0f778b711351306
Kerensivira <92549819+Kerensivira@users.noreply.github.com>
2021-11-20 10:26:51 -0500

    .

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ elCalcular.addEventListener("click", () => {
 
 let elMonto = document.getElementById("monto").Value;
 //alert(elMonto);
+
 console.log (elMonto);
 
 //document.getElementById("Divisas1").value;
