efc189efdbe8ace8c3088c81bcb08c389b7c5414
Kerensivira <92549819+Kerensivira@users.noreply.github.com>
2021-11-20 10:59:33 -0500

    0000

--- script.js
+++ script.js
@@ -7,13 +7,25 @@ function convertir() {
         let elMonto = document.getElementById("monto").value;
         let MonedaLocal = document.getElementById("Divisas1").value;
         let MonedaDestino = document.getElementById("Divisas2").value;
+        let elResultado = 0
 
         console.log(elMonto);
         console.log(MonedaLocal);
         console.log(MonedaDestino);
+
+        if (MonedaLocal == "COP (Peso Colombiano)") {
+            if (MonedaDestino == COP(PesoColombiano)) {
+                elResultado = elMonto;
+            }
+        }
+        if (MonedaLocal == "EUR (Euro)") {
+            {
+                elResultado = elMonto * 4.442, 50;
+            }
+        }
     })
 }
 
 
 
-//document.getElementById("Divisas1").value;
+
