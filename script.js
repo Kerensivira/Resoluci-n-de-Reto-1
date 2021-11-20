1f361e6950611b7a4dc6120eb0c01c9744dd8b07
Kerensivira <92549819+Kerensivira@users.noreply.github.com>
2021-11-20 10:00:02 -0500

    5

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+let elCalcular = document.getElementById("calcular")
+
 let elMonto = document.getElementById("monto").Value;
 alert(elMonto);
 console.log ("hello")
-document.getElementById("Divisas1").value;
+
+//document.getElementById("Divisas1").value;
