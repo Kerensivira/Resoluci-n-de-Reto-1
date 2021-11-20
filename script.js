7fb08e3d4733558a14735eeb46c552803a2ddb5c
Kerensivira <92549819+Kerensivira@users.noreply.github.com>
2021-11-20 09:51:13 -0500

    1

--- script.js
+++ script.js
@@ -1,42 +1,5 @@
 let elMonto = document.getElementById("monto").Value;
 alert(elMonto);
 console.log ("hello")
-
-//convertir();
-//function convertir() {
-   // let elIdCalcular = document.getElementById("calcular");
-   // elIdCalcular.addEventListener("click", () => {
-     //   console.log("estoy convirtiendo");
-    //}
-        // capturar el valor de "elIdCalcular": elIdCalcular.value
-
-//const getCalcular = () => {
-  //      let elIdCalcular = document.getElementById("calcular").value;
-    //    document.getElementById("calcular").innerHTML = inputValue
-   // }
-    // capturar la moneda inicial (local)
-
-    //const getDivisas1 = () => {
-       // let elIdDivisas1 = document.getElementById("Divisas1").value;
-       // document.getElementById("Divisas1").innerHTML = inputValue
-  //  }
-
-    // captura la moneda final (destino)
-
-    //const getDivisas2 = () => {
-        //let elIdDivisas1 = document.getElementById("Divisas2").value;
-       // document.getElementById("Divisas2").innerHTML = inputValue
-
-       // let valore = ["resultado", "COP", "EUR", "GBP", "MXN", "USD"];
-       // let valore = parseInt(document.getElementById("monto").value);
-       // let resultado = 0;
-       // let COP = 10;
-       // let EUR = 20;
-       // let GBP = 10;
-        //let MXN = 20;
-        //let USD = 50;
-    //}
-
-    //COP A TODAS LAS MONEDAS.
-
-   
+cument.getElementById("Divisas1").value;
+    
